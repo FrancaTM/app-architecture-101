86b55db121a53a88a9de727e62327881424efe8d
Túlio <francatm@outlook.com>
2019-05-25 10:54:36 -0300

    add data for asynchronous network call simulation

---
+++ lib/repository.dart
@@ -0,0 +1,8 @@
+import 'user.dart';
+
+class Repository {
+  Future<User> getUser() async {
+    await Future.delayed(Duration(seconds: 2));
+    return User(name: 'John', surname: 'Smith');
+  }
+}
---
+++ lib/user.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final String name;
+  final String surname;
+
+  User({@required this.name, @required this.surname});
+}
